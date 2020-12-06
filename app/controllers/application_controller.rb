70ca718678d4e690d76e89c840d6dad8bf603754
Navin Nair K <navin.kottayil@gmail.com>
2020-12-07 00:05:32 +0530

    fix: error message added for 500

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,8 +11,13 @@ class ApplicationController < ActionController::API
   end
 
   def handle_error (exception)
-    http_code = (exception.message =~ /invalid_credentials/) ? 403 : 500
-    render_json(http_code)
+    http_code, error =
+    if (exception.message =~ /invalid_credentials/)
+      [403, '']
+    else
+      [500, 'unknown failure']
+    end
+    render_json(http_code, error)
   end
 
   # render json out

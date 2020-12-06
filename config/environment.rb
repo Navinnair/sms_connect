773eef37b217c8096e7b737bbe6718f0101d5f36
Navin Nair K <navin.kottayil@gmail.com>
2020-12-06 16:16:19 +0530

    feat: disable pluralize_table_names to accomadate psql dump data

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the Rails application.
 require_relative 'application'
 
+# pluralize_table_names, db dump consists of db with singular name
+ActiveRecord::Base.pluralize_table_names = false
+
 # Initialize the Rails application.
 Rails.application.initialize!
+

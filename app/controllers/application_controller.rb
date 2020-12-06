9a5e6d57b852b62d20d0d7a8216eadb14d7a943a
Navin Nair K <navin.kottayil@gmail.com>
2020-12-06 17:46:52 +0530

    feat: api authentication implemented

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate
+
+  rescue_from Exception, with: :handle_error
+
+  private
+  # Authenticate using the username and auth_id
+  def authenticate
+    @current_user = Account.authenticate_user(request.authorization)
+  end
+
+  def handle_error (exception)
+    http_code = (exception.message =~ /invalid_credentials/) ? 403 : 500
+    render_json(http_code)
+  end
+
+  # render json out
+  def render_json(http_code, error = '', message = '')
+    render json: {message: message, error: error}, status: http_code
+  end
 end
--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,18 @@
 class Account < ApplicationRecord
   has_many :phone_numbers
+
+  class << self
+    # account authentication
+    def authenticate_user(encoded_auth_details)
+      username, auth_id = ::Base64.decode64(encoded_auth_details.split(' ', 2).last || '').split(/:/, 2)
+      authenticate(username, auth_id)
+    end
+
+    def authenticate(username, auth_id)
+      user = find_by(username: username, auth_id: auth_id)
+      raise 'invalid_credentials' unless user
+
+      user
+    end
+  end
 end
